e8dc632f5fd974d8ad39950156e91dfb459210ba
0x416c616e <alancompsci@gmail.com>
2025-11-02 22:24:17 -0600

    database tests pass, server tests do not

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,23 @@
 --see change in package.json, specifying -U postgres
+--and getting rid of -d filez
+
+--should be like this:
+--"db:schema": "psql -U postgres -f db/schema.sql",
 
 
+DROP DATABASE IF EXISTS filez;
+CREATE DATABASE filez;
+\c filez;
+
+CREATE TABLE folders (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+
+CREATE TABLE files (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    size integer NOT NULL,
+    folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+    UNIQUE (name, folder_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,21 @@
 import db from "#db/client";
+import { createFile } from "#db/queries/files";
+import { createFolder } from "#db/queries/folders";
 
-await db.connect();
-await seed();
-await db.end();
-console.log("🌱 Database seeded.");
+await initDb();
+
+async function initDb() {
+  await db.connect();
+  await seed();
+  await db.end();
+  console.log("Database seeded.");
+}
 
 async function seed() {
-  // TODO
+  for (let i = 0; i < 3; i++) {
+    const folder = await createFolder("Folder " + (Number(i) + 1));
+    for (let j = 0; j < 5; j++) {
+      await createFile("File " + (j + 1), 1000 * (j + 1), folder.id);
+    }
+  }
 }
